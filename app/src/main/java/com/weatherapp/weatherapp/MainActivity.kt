5898d686aa8383042f642aa559a9b4392b4381bf
Paulo Trujillo <36678251+devPaulo17@users.noreply.github.com>
2022-05-03 02:08:11 -0500

    [Add] UI test to search screen

--- app/src/main/java/com/weatherapp/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/weatherapp/MainActivity.kt
@@ -4,13 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.weatherapp.search.SearchActivity
-import com.weatherapp.weather.WeatherDetailActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this,WeatherDetailActivity::class.java))
+        startActivity(Intent(this,SearchActivity::class.java))
     }
 }
